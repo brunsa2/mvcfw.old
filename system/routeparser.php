bbf506ed36a0b3be4447b1bbd0457ca7a2edf895
Jeff Stubler <brunsa2@gmail.com>
2011-08-27 14:03:40 -0500

    added syntax-directed translation to calculate the regexes and symbols for url matching

--- system/routeparser.php
+++ system/routeparser.php
@@ -13,7 +13,7 @@ class RouteParser {
     
     public function parse() {
         $this->xml .= "<goal>\n";
-        $this->parseUrlSpecification();
+        $urlSpecification = $this->parseUrlSpecification();
         $this->match('EndToken');
         $this->xml .= "</goal>\n";
         file_put_contents(ROOT_DIRECTORY . DS . SYSTEM_DIRECTORY . DS . sha1($this->route) . '.xml', $this->xml);
@@ -21,27 +21,58 @@ class RouteParser {
     
     private function parseUrlSpecification() {
         $this->xml .= "<url_specification>\n";
-        $this->parseList($level + 1);
+        $list = $this->parseList($level + 1);
+        $urlSpecification = array();
+        $urlSpecification['regexes'] = $list['regexes'];
+        array_push($urlSpecification['regexes'], end($list['regexes']) . '\/');
+        $urlSpecification['symbols'] = $list['symbols'];
+        echo '<pre>' . print_r($urlSpecification, true) . '</pre><br /><br /><br />';
         $this->xml .= "</url_specification>\n";
+        return $urlSpecification;
     }
     
     private function parseList() {
         $this->xml .= "<list>\n";
+        $list = array();
+        $list['regexes'] = array();
         if($this->tokens[0]->isNot('EndToken')) {
-            $this->parseItem();
-            $this->parseList();
+            $item = $item = $this->parseItem();
+            $list1 = $this->parseList();
+            $list['regexes'] = array();
+            array_push($list['regexes'], $item['value']);
+            foreach($list1['regexes'] as $regex) {
+                array_push($list['regexes'], $item['value'] . $regex);
+            }
+            $list['symbols'] = array();
+            if(array_key_exists('symbol', $item)) {
+                array_push($list['symbols'], $item['symbol']);
+            }
+            foreach($list1['symbols'] as $symbol) {
+                array_push($list['symbols'], $symbol);
+            }
+            echo '<pre>' . print_r($list, true) . '</pre><br /><br /><br />';
+        } else {
+            $list['regexes'] = array();
+            $list['symbols'] = array();
         }
         $this->xml .= "</list>\n";
+        return $list;
     }
     
     private function parseItem() {
         $this->xml .= "<item>\n";
+        $item = array();
         if($this->tokens[0]->is('PlainTextToken')) {
-            $this->match('PlainTextToken');
+            $token = $this->match('PlainTextToken');
+            $item['value'] = $token->getText();
         } else if($this->tokens[0]->is('SlashToken')) {
             $this->match('SlashToken');
+            $item['value'] = '\/';
+            return $item;
         } else if($this->tokens[0]->is('OpeningParenthesisToken')) {
-            $this->parsePlaceholder();
+            $placeholder = $this->parsePlaceholder();
+            $item['value'] = $placeholder['value'];
+            $item['symbol'] = $placeholder['symbol'];
         } else {
             file_put_contents(ROOT_DIRECTORY . DS . SYSTEM_DIRECTORY . DS . sha1($this->route) . '.xml', $this->xml);
             global $errorHandler;
@@ -49,24 +80,36 @@ class RouteParser {
             exit;
         }
         $this->xml .= "</item>\n";
+        return $item;
     }
     
     private function parsePlaceholder() {
         $this->xml .= "<placeholder>\n";
+        $placeholder = array();
         $this->match('OpeningParenthesisToken');
-        $this->parsePlaceholderContents();
+        $placeholderContents = $this->parsePlaceholderContents();
+        $placeholder['value'] = $placeholderContents['value'];
+        $placeholder['symbol'] = $placeholderContents['symbol'];
         $this->match('ClosingParenthesisToken');
         $this->xml .= "</placeholder>\n";
+        return $placeholder;
     }
     
     private function parsePlaceholderContents() {
         $this->xml .= "<placeholder_contents>\n";
+        $placeholderContents = array();
         if($this->tokens[0]->is('AsteriskToken')) {
-            $this->parseAbsorbingPlaceholder();
+            $absorbingPlaceholder = $this->parseAbsorbingPlaceholder();
+            $placeholderContents['value'] = '([A-Za-z0-9\/]+)';
+            $placeholderContents['symbol'] = $absorbingPlaceholder['symbol'];
         } else if($this->tokens[0]->is('PlusSignToken')) {
-            $this->parseOptionalPlaceholder();
+            $optionalPlaceholder = $this->parseOptionalPlaceholder();
+            $placeholderContents['value'] = '([A-Za-z0-9]+)';
+            $placeholderContents['symbol'] = $optionalPlaceholder['symbol'];
         } else if($this->tokens[0]->is('PlainTextToken')) {
-            $this->match('PlainTextToken');
+            $token = $this->match('PlainTextToken');
+            $placeholderContents['value'] = '([A-Za-z0-9]+)';
+            $placeholderContents['symbol'] = array($token->getText(), 'required');
         } else {
             file_put_contents(ROOT_DIRECTORY . DS . SYSTEM_DIRECTORY . DS . sha1($this->route) . '.xml', $this->xml);
             global $errorHandler;
@@ -74,20 +117,27 @@ class RouteParser {
             exit;
         }
         $this->xml .= "</placeholder_contents>\n";
+        return $placeholderContents;
     }
     
     private function parseAbsorbingPlaceholder() {
         $this->xml .= "<absorbing_placeholder>\n";
         $this->match('AsteriskToken');
-        $this->match('PlainTextToken');
+        $token = $this->match('PlainTextToken');
+        $absorbingPlaceholder = array();
+        $absorbingPlaceholder['symbol'] = array($token->getText(), 'optional');
         $this->xml .= "</absorbing_placeholder>\n";
+        return $absorbingPlaceholder;
     }
     
     private function parseOptionalPlaceholder() {
         $this->xml .= "<optional_placeholder>\n";
         $this->match('PlusSignToken');
-        $this->match('PlainTextToken');
+        $token = $this->match('PlainTextToken');
+        $optionalPlaceholder = array();
+        $optionalPlaceholder['symbol'] = array($token->getText(), 'optional');
         $this->xml .= "</optional_placeholder>\n";
+        return $optionalPlaceholder;
     }
     
     public function match($tokenClass) {
@@ -95,7 +145,7 @@ class RouteParser {
         if($this->tokens[0]->is($tokenClass)) {
             $this->xml .= "<matched token=\"" . $this->tokens[0] . "\" />\n";
             $this->xml .= "</match>\n";
-            array_shift($this->tokens);
+            return array_shift($this->tokens);
         } else {
             file_put_contents(ROOT_DIRECTORY . DS . SYSTEM_DIRECTORY . DS . sha1($this->route) . '.xml', $this->xml);
             global $errorHandler;
