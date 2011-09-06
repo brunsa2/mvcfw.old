5c97745b3bc0fb25d60921ff0ad174ec48c1092b
Jeff Stubler <brunsa2@gmail.com>
2011-09-06 13:32:54 -0500

    dispatcher can dispatch route with sample arguments and dump debug info

--- system/dispatcher.php
+++ system/dispatcher.php
@@ -6,9 +6,15 @@ class Dispatcher {
             require_once(ROOT_DIRECTORY . DS . CONTROLLER_DIRECTORY . DS . strtolower($route['controller']) . '_controller.php');
             
             $controller = new ReflectionClass($route['controller'] . 'Controller');
+            $controllerInstance = $controller->newInstance();
+            $action = $controller->getMethod($route['action']);
+            echo '<h1>Action</h1><pre>' . print_r($action, true). '</pre>';
+            $parameters = $action->getParameters();
+            echo '<h1>Parameters</h1><pre>' . print_r($parameters, true) . '</pre>';
+            $action->invoke($controllerInstance, 'id');
         } else {
             global $errorHandler;
-            $errorHandler->shutdown('Controller was not found');
+            $errorHandler->shutdown('Controller file was not found');
         }
     }
 }
